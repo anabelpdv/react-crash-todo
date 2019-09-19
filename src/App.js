f85a7ed1446742d2132048b6a1f0d15ad64516a0
Anabel Perez <anabelpdv@gmail.com>
2019-09-19 15:20:11 -0400

    Finish addTodo

--- src/App.js
+++ src/App.js
@@ -4,23 +4,24 @@ import AddTodo from './components/AddTodo.js';
 import './App.css';
 import TodoItem from './components/TodoItem.js';
 import Header from './components/layout/header';
+import uuid from 'uuid';
 
 
 class App extends React.Component {
   state = {
     todos: [
       {
-        id:1,
+        id:uuid.v4(),
         title: 'Take out the trash',
         completed: false,
       },
       {
-        id:2,
+        id:uuid.v4(),
         title: 'Dinner with wife',
         completed: false,
       },
       {
-        id:3,
+        id:uuid.v4(),
         title: 'Meeting with boss',
         completed: false,
       }
@@ -41,13 +42,22 @@ class App extends React.Component {
     this.setState({todos:[...this.state.todos.filter(todo=> todo.id !== id
     )]});
   }
-
+  addTodo=(title)=>{
+    const newTodo = {
+      id: uuid.v4(),
+      title,
+      completed:false,
+    }
+    this.setState({todos: [...this.state.todos,newTodo]})
+  }
   render(){
     return (
       <div className="App">
+        <div className="container">
         <Header/>
-        <AddTodo />
+        <AddTodo  addTodo={this.addTodo}/>
         <Todos todos = {this.state.todos} markComplete={this.markComplete} delTodo={this.delTodo}/>
+        </div>
       </div>
     );
   }
